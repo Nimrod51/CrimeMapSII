a7e6e7e7ee232664f339db93bf598d5296b3a6a8
Nimrod Gavish <Nimrod Gavish>
2017-02-25 06:34:57 -0800

    Check

--- js/map.js
+++ js/map.js
@@ -1,5 +1,7 @@
 var heat; //Make heat map variable global
 
+var check;
+
 var mbAttr = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
 			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
